ee63ce3c0474cfc2a5d595ab9648adf294d45758
Samuel Dudley <sbarkerdudley@gmail.com>
2022-01-17 16:03:51 -0800

    Memoized context to stabilize and eliminate unnecessary re-renders

--- src/app/components/JobSearch/JobSearchContext.jsx
+++ src/app/components/JobSearch/JobSearchContext.jsx
@@ -1,12 +1,9 @@
-/* eslint-disable */
 import React, { createContext, useState, useEffect } from 'react';
-import {
-  useLocation, Link,
-} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export const JobSearchContext = createContext(null);
 
-const JobSearchProvider = ({children}) => {
+const JobSearchProvider = ({ children }) => {
   const [range, setRange] = useState(null);
   const [drawer, setDrawer] = useState(false);
   const [keywords, setKeywords] = useState('');
@@ -19,7 +16,6 @@ const JobSearchProvider = ({children}) => {
   const [employmentType, setEmploymentType] = useState(null);
   const locationHook = useLocation();
 
-  //set filters based on url
   useEffect(() => {
     const url = new URLSearchParams(locationHook.search);
     if (url.has('keywords')) setKeywords(url.get('keywords'));
@@ -28,32 +24,44 @@ const JobSearchProvider = ({children}) => {
     if (url.has('location')) setLocation(url.get('location'));
     if (url.has('experienceLevel')) setExperience(url.get('experienceLevel'));
     if (url.has('employmentType')) setWorkSite(url.get('employmentType'));
-  }, []);
+  }, [locationHook.search]);
+
+  const value = React.useMemo(() => ({
+    range,
+    setRange,
+    sort,
+    setSort,
+    drawer,
+    setDrawer,
+    salary,
+    setSalary,
+    workSite,
+    setWorkSite,
+    location,
+    setLocation,
+    keywords,
+    setKeywords,
+    datePosted,
+    setDatePosted,
+    experience,
+    setExperience,
+    employmentType,
+    setEmploymentType,
+  }), [
+    range,
+    sort,
+    drawer,
+    salary,
+    workSite,
+    location,
+    keywords,
+    datePosted,
+    experience,
+    employmentType,
+  ]);
 
   return (
-    <JobSearchContext.Provider value={{
-      range,
-      setRange,
-      sort,
-      setSort,
-      drawer,
-      setDrawer,
-      salary,
-      setSalary,
-      workSite,
-      setWorkSite,
-      location,
-      setLocation,
-      keywords,
-      setKeywords,
-      datePosted,
-      setDatePosted,
-      experience,
-      setExperience,
-      employmentType,
-      setEmploymentType,
-    }}
-    >
+    <JobSearchContext.Provider value={value}>
       {children}
     </JobSearchContext.Provider>
   );
--- src/app/pages/LogIn.jsx
+++ src/app/pages/LogIn.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
+import { Input, Stack, Typography } from '@mui/material';
+import PrimaryButton from '../components/PrimaryButton';
 
 const LogIn = () => {
   const [loginUsername, setLoginUsername] = useState('');
@@ -17,21 +19,27 @@ const LogIn = () => {
   };
 
   return (
-    <div className="App">
-
-      <div>
-        <h1>Login</h1>
-        <input
-          placeholder="username"
-          onChange={(e) => setLoginUsername(e.target.value)}
-        />
-        <input
-          placeholder="password"
-          onChange={(e) => setLoginPassword(e.target.value)}
-        />
-        <button type="submit" onClick={login}>Submit</button>
-      </div>
-    </div>
+    <Stack
+      direction="column"
+      sx={{ p: 2, m: 2 }}
+    >
+      <Typography variant="h3">
+        Login
+      </Typography>
+      <br />
+      <Input
+        placeholder="username"
+        onChange={(e) => setLoginUsername(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        onChange={(e) => setLoginPassword(e.target.value)}
+      />
+      <PrimaryButton
+        onClick={login}
+        text="Login"
+      />
+    </Stack>
   );
 };
 
